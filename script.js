b6e6c816c1b3e30d3589a0e88ef63b964c1c5526
Azhaghiri <ganadhan6528@gmail.com>
2024-10-24 18:53:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //your JS code here. If required.
-let counterValue = 0;
+let counterValue = 1;
 
 let counter = document.getElementById("counter");
 let incrementBtn = document.getElementById("incrementBtn");
 
 incrementBtn.addEventListener("click",function () {
-	counterValue++;
 	alert('Current value before increment: ' + counterValue)
  
+	counterValue++;
 
 	counter.textContent = counterValue;
 })
